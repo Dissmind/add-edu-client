790f385afe09f93bf8eab4ea1c9b0f64d8c5dcce
Dissmind <DissMind@gmail.com>
2021-05-05 01:25:52 +0200

    [ADD](catalog): init item mock

---
+++ src/features/catalogs/test/mocks/items.mock.ts
@@ -0,0 +1,8 @@
+import {Items} from "../../components/list";
+
+
+export const itemsArticlesMock: Array<Items> = [
+  {id: 1, title: 'PHP (Переменные и типы данных)'},
+  {id: 2, title: 'PHP (Операторы)'},
+  {id: 3, title: 'PHP (Условные операторы)'}
+]
